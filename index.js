02a4563c82c97cf6b2c7542c4a8f6d05c1907701
Desmonne Dotson <brazette97@gmail.com>
2023-12-13 11:54:13 -0600

    creating an object

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+const employee = {
+    name: "Tom",
+    streetAddress: "123 Main St",
+  };
+function updateEmployeeWithKeyAndValue (name, key, value) {
+    return {...employee, [key]: value};
+}
+const updatedEmployee = updateEmployeeWithKeyAndValue(employee, "name", "Tom");
+console.log(updatedEmployee);
+function destructivelyUpdateEmployeeWithKeyAndValue (employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey (employee, key) {
+    const newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey (employee, key) {
+    delete employee[key];
+    return employee;
+}
